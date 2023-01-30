cc2eea463e0f8676ca87734d74e863fe008de560
stamparkour <42157566+stamparkour@users.noreply.github.com>
2023-01-30 12:40:32 -0500

    Create Command.java

---
+++ src/main/java/frc/robot/commands/Command.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Command extends CommandBase {
+    public SubsystemBase base;
+
+    public Command() {
+        
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public void end(boolean ending) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
