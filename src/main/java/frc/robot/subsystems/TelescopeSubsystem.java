6cc8d31a97d452b79f46510d4fda2864c47ff617
vhrlaptop1 <94510641+vhrlaptop1@users.noreply.github.com>
2023-02-01 19:05:35 -0800

    Create TelescopeSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/TelescopeSubsystem.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+import edu.wpi.first.math.controller.PIDController;
+
+import frc.robot.Constants;
+
+
+public class TelescopeSubsystem {
+   
+    
+    /** Create Variables: Motor, Encoder, PID */
+    final CANSparkMax motor = new CANSparkMax(Constants.armMotorID, MotorType.kBrushless);
+    final SparkMaxAbsoluteEncoder encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
+    final PIDController pid = new PIDController(getCurrentArmLength(), getEncoderInRadians(), getCurrentArmLength());
+    
+    /** Using Radius, Convert Length To Radians (length/radius = radians) */
+    public double getLengthToRadians(double length){
+        return (length / Constants.ArmWinchRadius);
+    }
+    /** Convert Radians Back To Length (radians * radius = length) */
+    public double getRadiansToLength(double radians){
+        return (radians * Constants.ArmWinchRadius);
+    }
+    /** Convert Encoder Position Into Radians (encoder_position * 2 * pi) */
+    public double getEncoderInRadians() {
+        return encoder.getPosition() * 2 * Math.PI;
+    }
+    /** The Equation Uses "getEncoderInRadians" As The Input For The Equation "getRadiansToLength" */
+    public double getCurrentArmLength(){
+        return getRadiansToLength(getEncoderInRadians());
+    }
+    /** Using A PID, Calculate The Encoder Position And Create A Certain Setpoint */
+    public void setpid (double setpoint){
+        motor.set(pid.calculate(encoder.getPosition(), setpoint));
+    }
+    
+}
+
