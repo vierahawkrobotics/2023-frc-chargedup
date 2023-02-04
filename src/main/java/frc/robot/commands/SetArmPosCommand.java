4679fe8e0b0605b5009ddaeee2ac75f87306bc1d
Hawk Robotics <Hawk Robotics@DESKTOP-BDSFGP6.local>
2023-02-04 16:10:47 -0500

    Update SetArmPosCommand.java

--- src/main/java/frc/robot/commands/SetArmPosCommand.java
+++ src/main/java/frc/robot/commands/SetArmPosCommand.java
@@ -1,36 +1,46 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.net.SocketTimeoutException;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
-public class SetArmPosCommand extends CommandBase {
-    public SubsystemBase base;
 
-    public SetArmPosCommand() {
-        requires(Robot.ArmSubsystem);
+
+public class SetArmPosCommand extends CommandBase {
+    public ArmSubsystem arm_subsystem;
+    public double m_height;
+    public SetArmPosCommand(double height) {
+        addRequirements(arm_subsystem);
+       m_height = height;
     }
 
     @Override
     public void initialize() {
 
+
     }
 
     @Override
     public void execute() {
-        double rotateSpeed = RobotContainer.driverController.getRawAxis(Constants.DRIVER_CONTROLLER_ROTATE_AXIS);
-        motor.set()
+        
+        double radians = ArmSubsystem.getHeightToRadians(m_height);
+        ArmSubsystem.setTargetRotation(radians);
+
     }
 
     @Override
     public void end(boolean ending) {
-
+        ArmSubsystem.motor.set(0.0);
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        
     }
 }
