ab4e147086ed712681065fdb637c8e34695cf3b2
Hawk Robotics <Hawk Robotics@DESKTOP-BDSFGP6.local>
2023-02-06 18:53:57 -0500

    Update Command.java and SetTelescopePosition.java

---
+++ src/main/java/frc/robot/commands/SetTelescopePosition.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.TelescopeSubsystem;
+
+public class SetTelescopePosition extends CommandBase {
+    public SubsystemBase base;
+    public TelescopeSubsystem telescope_subsystem;
+    public double distance;
+    public SetTelescopePosition(double m_distance, TelescopeSubsystem t_subsystem) {
+        this.telescope_subsystem = t_subsystem;
+        addRequirements(telescope_subsystem);
+        m_distance = distance;
+    }
+
+    
+
+    @Override
+    public void initialize() {
+        double setpoint = telescope_subsystem.getLengthToRadians(distance);
+        telescope_subsystem.setpid(setpoint);
+    }
+
+    @Override
+    public void execute() {
+        double setpoint = telescope_subsystem.getLengthToRadians(distance);
+        telescope_subsystem.setpid(setpoint);
+    }
+
+    @Override
+    public void end(boolean ending) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
